022e81df7563898a872e3ac990947ca9168a08a6
AbleGod766 <godable766@gmail.com>
2026-07-09 06:33:26 -0700

    Harden Vercel WSGI entrypoint

--- api/index.py
+++ api/index.py
@@ -8,10 +8,13 @@ sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luxe_project.settings")
 
+import django
 from django.conf import settings
 from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
+
+django.setup()
 
 if (
     os.environ.get("VERCEL_ENV")
@@ -25,4 +28,5 @@ if (
     call_command("migrate", interactive=False, verbosity=0)
 
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
